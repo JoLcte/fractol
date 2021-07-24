133745273064ce570fd1907a562cda570d2de332
JoLcte <lecomte.jo@hotmail.fr>
2021-07-24 14:45:12 +0200

    w/out the printf

--- src/key_move.c
+++ src/key_move.c
@@ -6,7 +6,7 @@
 /*   By: jlecomte <jlecomte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 00:26:22 by jlecomte          #+#    #+#             */
-/*   Updated: 2021/07/24 13:22:09 by jlecomte         ###   ########.fr       */
+/*   Updated: 2021/07/24 14:44:47 by jlecomte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	move_up(t_data *data)
 
 	g = data->g;
 	g->ul[Y] += 10 * g->factor;
-	printf("ul[X] = %f\nul[Y] = %f\n", g->ul[X], g->ul[Y]);
 	g->center[Y] += 10 * g->factor;
 	if (g->set > 0)
 		julia_loop(data);
@@ -35,7 +34,6 @@ void	move_down(t_data *data)
 
 	g = data->g;
 	g->ul[Y] -= 10 * g->factor;
-	printf("ul[X] = %f\nul[Y] = %f\n", g->ul[X], g->ul[Y]);
 	g->center[Y] -= 10 * g->factor;
 	if (g->set > 0)
 		julia_loop(data);
@@ -52,7 +50,6 @@ void	move_left(t_data *data)
 
 	g = data->g;
 	g->ul[X] -= 10 * g->factor;
-	printf("ul[X] = %f\nul[Y] = %f\n", g->ul[X], g->ul[Y]);
 	g->center[X] -= 10 * g->factor;
 	if (g->set > 0)
 		julia_loop(data);
